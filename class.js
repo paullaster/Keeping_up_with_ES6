016edd9c8881893590c9893d90a10e348d65bed3
paullaster <paullasterokoth98@gmail.com>
2022-02-14 05:02:15 +0300

    inheritance of classes

--- class.js
+++ class.js
@@ -4,8 +4,53 @@ class Person{
         this.age = age;
     }
     greetings(){
-        console.log(`Hi, ${this.name}, today you're ${this.age} years old`)
+        return (`Hi, ${this.name}, today you're ${this.age} years old`)
     }
 }
-const edu = new Person("Edward", 24);
-edu.greetings()
+
+class Student extends Person{
+    constructor(name, age, level, course){
+        super(name,age);
+        this.level = level;
+        this.course = course;
+    }
+    studentInformation(){
+        const inheritedString = super.greetings();
+        console.log(`${inheritedString} a level ${this.level}  ${this.course} student `);
+    }
+}
+const edu = new Student("Edward", 24, "4", "BSc. IT");
+edu.studentInformation()
+
+class Customer extends Person{
+    constructor({name ="customer",  age = "n/a", contactMethod}){
+        super(name, age);
+        this.contactMethod = contactMethod;
+        this.accountCredit = null;
+    }
+    addCredit(amount){
+        this.accountCredit += amount;
+    }
+    reduceCredit(amount){
+        this.accountCredit -= amount;
+    }
+    static salutation(){
+        console.log("Hello Hi");
+    }
+}
+
+const customer1 = new Customer({contactMethod:"Email"});
+const customer2 = new Customer({name:"Wiscon O", contactMethod: "mobile"});
+console.log(customer1);
+console.log(customer2);
+console.log(customer1.accountCredit);
+console.log(customer2.accountCredit);
+customer1.addCredit(5000);
+customer2.addCredit(10000);
+customer2.addCredit(50000);
+console.log(customer1.accountCredit);
+console.log(customer2.accountCredit);
+customer1.reduceCredit(800);
+customer2.reduceCredit(800);
+console.log(customer1.accountCredit);
+console.log(customer2.accountCredit);
